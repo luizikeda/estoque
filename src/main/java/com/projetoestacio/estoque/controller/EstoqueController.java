c88e05d1e0d3b01be1e5ebffb297454cfcebfed9
luiz ikeda <luizmiguelikeda11@outlook.com>
2023-08-28 15:54:58 -0300

    main

---
+++ src/main/java/com/projetoestacio/estoque/controller/EstoqueController.java
@@ -0,0 +1,7 @@
+package com.projetoestacio.estoque.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EstoqueController {
+}
---
+++ src/main/java/com/projetoestacio/estoque/model/ProdutoModel.java
@@ -0,0 +1,8 @@
+package com.projetoestacio.estoque.model;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class ProdutoModel {
+}
