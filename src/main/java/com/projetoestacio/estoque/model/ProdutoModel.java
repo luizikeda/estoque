923cb9b8d6438c40c1bea772a856fa47e51f0583
luiz ikeda <luizmiguelikeda11@outlook.com>
2023-08-29 11:35:28 -0300

    main

--- src/main/java/com/projetoestacio/estoque/model/ProdutoModel.java
+++ src/main/java/com/projetoestacio/estoque/model/ProdutoModel.java
@@ -1,8 +1,18 @@
 package com.projetoestacio.estoque.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class ProdutoModel {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
 }
