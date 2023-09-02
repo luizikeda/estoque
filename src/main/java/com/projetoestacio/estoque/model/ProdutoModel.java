eb119cf1d260974b330443cdf9482be7f748a7a7
luiz ikeda <luizmiguelikeda11@outlook.com>
2023-09-02 17:42:20 -0300

    entidade criada

--- src/main/java/com/projetoestacio/estoque/model/ProdutoModel.java
+++ src/main/java/com/projetoestacio/estoque/model/ProdutoModel.java
@@ -1,13 +1,13 @@
 package com.projetoestacio.estoque.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.util.Date;
+
 @Entity
+@Table(name = "produto")
 public class ProdutoModel {
 
     @Id
@@ -15,4 +15,52 @@ public class ProdutoModel {
     @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
+
+    private String nome;
+
+    private String sku;
+
+    private String valor;
+
+    private Date datavalidade;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Date getDatavalidade() {
+        return datavalidade;
+    }
+
+    public void setDatavalidade(Date datavalidade) {
+        this.datavalidade = datavalidade;
+    }
 }
