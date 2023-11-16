d3afadc33483ca906ab513a4b2748dbe22b68731
PedroBarrak <pedro.barraqui@gmail.com>
2023-11-15 22:40:28 -0300

    Deleter VendaProduto adicionado. (Pedro Barraqui)

--- src/main/java/com/projetoestacio/estoque/controller/VendasController.java
+++ src/main/java/com/projetoestacio/estoque/controller/VendasController.java
@@ -3,7 +3,10 @@ package com.projetoestacio.estoque.controller;
 import com.projetoestacio.estoque.dto.ProdutoQuantidadeRequest;
 import com.projetoestacio.estoque.interfaces.*;
 import com.projetoestacio.estoque.model.Venda;
+import com.projetoestacio.estoque.repository.VendasDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -14,27 +17,38 @@ public class VendasController{
     @Autowired
      IVendasService vendaService;
 
-    // Criar nova venda:
+    @Autowired
+    VendasDAO vendasDAO;
+
     @PostMapping
     public Venda criarVenda(@RequestBody Venda venda) {
         return vendaService.criarVenda(venda);
     }
 
-    // Adicionar um produto a uma venda:
     @PostMapping("/{vendaId}/adicionarProduto")
     public Venda adicionarProdutoNaVenda(@PathVariable String vendaId, @RequestBody ProdutoQuantidadeRequest produtoQuantidadeRequest) {
         return vendaService.adicionarProdutoNaVenda(vendaId, produtoQuantidadeRequest);
     }
 
-    // Obter uma venda por ID:
     @GetMapping("/{id}")
     public Venda obterVendaPorId(@PathVariable String id) {
         return vendaService.obterVendaPorId(id);
     }
 
-    // Lista todas as vendas:
     @GetMapping
     public List<Venda> listarVendas() {
         return vendaService.listarVendas();
     }
+
+    @DeleteMapping(value = "/delete/{vendaId}", produces = "application/json")
+    public ResponseEntity excluirProdutoVenda(@PathVariable("vendaId") String vendaId){
+        try {
+            Venda venda = vendasDAO.findById(vendaId).orElse(null);
+            vendasDAO.delete(venda);
+            return new ResponseEntity("Produto Deletado Com Sucesso", HttpStatus.OK);
+        } catch (Exception outro) {
+            throw outro;
+        }
+    }
+
 }
